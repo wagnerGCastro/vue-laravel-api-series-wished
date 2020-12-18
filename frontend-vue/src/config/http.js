c8f2ff334bed552294ef1b80c5167b0760bb482b
Wagner Castro <wagner.castro.blueshop@gmail.com>
2020-12-18 15:07:26 -0300

    #06 Usando Mutations Types, para Commit por types
    - Exemplo ao fazer login

--- frontend-vue/src/config/http.js
+++ frontend-vue/src/config/http.js
@@ -6,3 +6,10 @@ Vue.use(VueResource)
 const http = Vue.http
 
 http.options.root = 'http://127.0.0.1:8010/api'
+
+const setBearerToken = token => {
+    http.headers.common['Authorization'] = `Bearer ${token}`
+}
+
+export default services
+export { http, setBearerToken }
