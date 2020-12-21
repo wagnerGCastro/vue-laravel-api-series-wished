e5eebf3af332111daa59736061a5d0dd84656349
Wagner Castro <wagner.castro.blueshop@gmail.com>
2020-12-21 17:30:19 -0300

    #12 Criando store para cada modules seprados - [Part 1]

--- frontend-vue/src/store/modules/auth/index.js
+++ frontend-vue/src/store/modules/auth/index.js
@@ -1,57 +1,12 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
-import { services, storage, setBearerToken } from '@/services'
-// console.log(typeof setBearerToken)
+export const Auth = {
+    namespaced: true,
 
-Vue.use(Vuex)
-
-/**
- * https://vuex.vuejs.org/ptbr/guide/mutations.html
- *  https://www.youtube.com/watch?v=qq8yJmXys6U
- * 
- * https://morioh.com/p/4bb19aa8ea3e
- */
-
-/**  
- *  Mutation Types
- */
-const SET_USER = 'SET_USER'
-const SET_TOKEN = 'SET_TOKEN'
-const SET_EXPIRES = 'SET_EXPIRES'
-
-export default new Vuex.Store({
     state: {
         user: {},
         token: '',
         expires: '',
-        count: 0,
-        todos: [
-            { id: 1, text: 'Nome 1', done: true },
-            { id: 2, text: 'Nome 2', done: false },
-            { id: 3, text: 'Nome 3', done: true },
-            { id: 4, text: 'Nome 4', done: false }
-        ]
     },
-    getters: {
-        doneTodos: state => {
-            return state.todos.filter(todo => todo.done)
-        },
-
-        doneTodosCount(state) {
-            return state.todos.filter(todo => todo.done).length
-        },
-
-        counter: state => state.count,
-        todos: state => state.todos,
-        getUser: state => state.user,
-        hasToken: ({ token }) => !!token
-    },
-    /**
-     * Exuta dados de forma sincrona, recomendado para alterar o estado da Aplicação
-     * 
-     * Mutações Devem Ser Síncronas
-     *  https://vuex.vuejs.org/ptbr/guide/mutations.html#mutacoes-devem-ser-sincronas
-     */
+  
     mutations: {
         [SET_TOKEN](state, token) {
             state.token = token
@@ -65,37 +20,9 @@ export default new Vuex.Store({
             state.expires = payload
         },
 
-        increment: state => state.count++,
-
-        decrement: (state, num) => {
-            console.log('decrementAction', num)
-            state.count--
-        }
     },
-    /**
-     * Recomendo para buscas do Servidor, API. Dispara acoes asincronas
-     */
+  
     actions: {
-        // setToken(context, token) {
-        //     context.dispatch('SET_TOKEN', token)
-        // }
-
-        decrement: context => context.commit('decrement'),
-        increment: ({ commit }) => commit('increment'),
-
-        incrementAction: context => context.commit('increment'),
-
-        /** Simulando uma chamada para API  */
-        decrementAction: ({ commit }) => {
-            // Algoritmo da API
-
-            // Apos o resultado da API, passando um parametro
-            setTimeout(() => {
-                //
-                commit('decrement', 4)
-            }, 2000)
-        },
-
         actionCheckToken: ({ dispatch, state }) => {
             if (state.token) {
                 return Promise.resolve(state.token)
@@ -158,8 +85,11 @@ export default new Vuex.Store({
             dispatch('actionSetToken', '')
             dispatch('actionSetExpires', '')
         }
-          
     },
-    modules: {
+  
+    getters: {
+        getUser: state => state.user,
+        hasToken: ({ token }) => !!token
     }
-})
+
+  }
---
+++ frontend-vue/src/store/modules/todos/index.js
@@ -0,0 +1,31 @@
+export const Todos = {
+    namespaced: true,
+
+    state: {
+        todos: [
+            { id: 1, text: 'Nome 1', done: true },
+            { id: 2, text: 'Nome 2', done: false },
+            { id: 3, text: 'Nome 3', done: true },
+            { id: 4, text: 'Nome 4', done: false }
+        ]
+    },
+  
+    mutations: {
+        //
+    },
+
+    getters: {
+        doneTodos: state => {
+            return state.todos.filter(todo => todo.done)
+        },
+
+        doneTodosCount(state) {
+            return state.todos.filter(todo => todo.done).length
+        },
+    },
+   
+    actions: {
+        //
+    }
+  
+}
